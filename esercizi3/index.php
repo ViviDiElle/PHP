040616e85a7389f2de76cc29df011d212cd4e669
ViviDiElle <dileonardov@yahoo.com>
2024-05-12 13:38:25 +0200

    php

---
+++ esercizi3/index.php
@@ -0,0 +1,96 @@
+<?php
+
+/* ES.1 
+Costruire un array di 5 elementi inserendo in ogni elemento il valore del suo
+indice.
+
+    $indici = [0, 1, 2, 3, 4];
+
+    var_dump($indici);
+
+    
+*/
+
+// ES.2 Create un array che contiene 10 numeri a piacere e sommate solo i numeri pari.
+
+/*$numeri = [8, 22, 82, 44, 11, 26, 3, 16, 75, 29];
+
+$arrayLength = count($numeri);
+
+$sommaPari = 0;
+
+for($i = 0; $i < $arrayLength; $i++) {
+
+    if($numeri[$i] % 2 == 0) {
+       
+        $sommaPari += $numeri[$i];
+    
+    }
+}
+
+echo "La somma dei numeri pari è: " . $sommaPari;
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ file1/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <?php
+    echo "Hello World!";
+
+    $nome = "Pippo";
+    echo $nome;
+
+    $eta = 20;
+    echo $eta;
+
+    echo "Ciao sono $nome e ho $eta anni <br>";
+    echo 'Ciao sono ' . $nome . ' e ho ' . $eta . 'anni';
+    echo 'ciao a tutte';
+
+    
+?>
+</body>
+</html>
