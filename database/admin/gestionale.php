6517dd002ba69e03ecb8772ae6d3c4d72b74878e
ViviDiElle <dileonardov@yahoo.com>
2024-07-04 23:28:52 +0200

    update_

--- database/admin/gestionale.php
+++ database/admin/gestionale.php
@@ -6,16 +6,8 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Gestionale Libri</h1>
-    <ul>
-        <li><a href="">Gestione Autori</a></li>
-        <li><a href="">Gestione Editori</a></li>
-        <li><a href="">Gestione Libri</a></li>
-    </ul>
-
-
-
-    <?php include 'partials/menu.html'?>
+<?php include 'partials/menu.html' ?>
+    
 </body>
 </html>
 
